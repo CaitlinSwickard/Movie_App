bb60ea0570c1205a343f223bd71ddf02b241d88e
Caitlin Swickard <caitlin6029@gmail.com>
2022-12-06 16:25:25 -0700

    attached data from Api call to dom elements

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=d38121c368bda37f4ee46886e0599a43&query="'
 
 
-// search form dom elements
+// dom elements
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+// 
 
 // get initial movies 
 getMovies(API_URL)
@@ -17,8 +20,45 @@ async function getMovies(url) {
   const res = await fetch(url)
   const data = await res.json()
 
-  console.log(data.results)
+  showMovies(data.results)
 }
+
+function showMovies(movies) {
+  main.innerHTML = ''
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+    movieEl.innerHTML = `
+    
+      <img src="${IMG_PATH + poster_path}" alt="${title}">
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+          ${overview}
+      </div>
+    `
+    main.appendChild(movieEl)
+  })
+}
+
+
+// function to get class color for rating
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green'
+  } else if (vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
+}
+
 
 // search entered into search bar event listner to hit API call and return search results
 form.addEventListener('submit', (e) => {
